4786a389760cc829d48b20aec30b5bc5f83856b3
John The Ripper <ankit.tyagiw@gmail.com>
2018-12-29 16:42:59 -0600

    Add files via upload

---
+++ MD5CollisionOrPartialCollision.py
@@ -0,0 +1,26 @@
+import hashlib
+import re
+
+with open('filename') as fh:
+               read_data =  fh.read()
+found = False
+loop_counter = 0
+
+def md5(data):
+	m = hashlib.md5()
+	m.update(str(data))
+	h = m.hexdigest()
+	return h
+
+while not found:
+	hash = md5(read_data + "\n" + str(loop_counter))
+	if (re.match("HASH or partial hash using REGEX", str(hash))):
+		found = True
+		print(hash)
+		with open('new filename', 'a') as fh:
+                	fh.write(read_data + "\n" + str(loop_counter))
+		print(read_data + "\n" + str(loop_counter))
+	else:
+		loop_counter += 1
+
+
